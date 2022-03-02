3cc7d69f01eda3ee11ec3a70a787295a89dea8b1
Dendi Suhubdy <gpuhashcracker@protonmail.com>
2022-03-02 11:55:31 +0700

    feat: train

--- skeleton/train_crypto_prices.py
+++ skeleton/train_crypto_prices.py
@@ -1,13 +1,17 @@
 import torch
+import numpy
+import pickle
 from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
 
-import pickle
-
 with open("./data/btc_usd_spot_data.pkl", 'rb') as f:
     prices_data = pickle.load(f)
+
+prices_data = numpy.array(prices_data)
+prices_data = torch.from_numpy(prices_data).unsqueeze(1)
+print(prices_data)
 
 trainset = torch.utils.data.TensorDataset(torch.tensor(prices_data))
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=4)
