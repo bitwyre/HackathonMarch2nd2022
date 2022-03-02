ed468b3b1f9198b2f1db6a0c9e848c51d2184cfa
Dendi Suhubdy <gpuhashcracker@protonmail.com>
2022-03-02 14:16:02 +0700

    fix: lstm

--- skeleton/train_crypto_prices.py
+++ skeleton/train_crypto_prices.py
@@ -9,9 +9,102 @@ from torchvision.transforms import ToTensor
 with open("./data/btc_usd_spot_data.pkl", 'rb') as f:
     prices_data = pickle.load(f)
 
-prices_data = numpy.array(prices_data)
-prices_data = torch.from_numpy(prices_data).unsqueeze(1)
-print(prices_data)
+# autoregressive dataset
 
-trainset = torch.utils.data.TensorDataset(torch.tensor(prices_data))
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=4)
+def build_lag_dataset(prices_data: list, lag: int):
+    """ Build a timeseries dataset from 
+        all the previous lags
+    """
+    train_data = []
+    # length_of_price_data = len(prices_data)
+    train_data = prices_data[lag:-lag]
+    print(f"New timeseries is length is {len(train_data)}")
+    return train_data
+
+lag = 100
+
+train_data = build_lag_dataset(prices_data, lag)
+train_data = numpy.array(train_data)
+prices_data = numpy.array(prices_data)
+# print(prices_data)
+
+test_data_size = 200
+
+label_data = prices_data[2*lag:]
+
+# convert to autoregressive input
+# an autoregressive input is the next sequence is predicted from
+# the previous sequence
+test_data = prices_data[-test_data_size:]
+
+print(f"Length of training set is {len(train_data)}")
+print(f"Length of label set is {len(label_data)}")
+print(f"Length of test set is {len(test_data)}")
+
+train_data = torch.Tensor(train_data).view(-1)
+label_data = torch.Tensor(label_data).view(-1)
+test_data = torch.Tensor(test_data).view(-1)
+
+batch_size = 64
+
+trainset = torch.utils.data.TensorDataset(train_data, label_data)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size)
+testset = torch.utils.data.TensorDataset(test_data)
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size)
+
+class LSTM(nn.Module):
+    def __init__(self, input_size=1, hidden_layer_size=100, output_size=1):
+        super().__init__()
+        self.hidden_layer_size = hidden_layer_size
+
+        self.lstm = nn.LSTM(input_size, hidden_layer_size)
+
+        self.linear = nn.Linear(hidden_layer_size, output_size)
+
+        self.hidden_cell = (torch.zeros(1,1,self.hidden_layer_size),
+                            torch.zeros(1,1,self.hidden_layer_size))
+
+    def forward(self, input_seq):
+        lstm_out, self.hidden_cell = self.lstm(input_seq.view(len(input_seq) ,1, -1), self.hidden_cell)
+        predictions = self.linear(lstm_out.view(len(input_seq), -1))
+        return predictions
+
+# Get cpu or gpu device for training.
+# device = "cuda" if torch.cuda.is_available() else "cpu"
+device = "cpu"
+print(f"Using {device} device")
+model = LSTM()
+model = model.to(device)
+loss_function = nn.MSELoss()
+loss_function = loss_function.to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+# torch.autograd.set_detect_anomaly(True)
+
+print(model)
+
+def train(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    model.train()
+    for batch, (X, y) in enumerate(dataloader):
+        X, y = X.to(device), y.to(device)
+
+        # Compute prediction error
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        # Backpropagation
+        optimizer.zero_grad()
+        loss.backward(retain_graph=True)
+        optimizer.step()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+epochs = 10
+for t in range(epochs):
+    print(f"Epoch {t+1}\n-------------------------------")
+    train(trainloader, model, loss_function, optimizer)
+    torch.save(model.state_dict(), "model_lstm.pth")
+    print("Saved PyTorch Model State to model.pth")
+print("Done!")
