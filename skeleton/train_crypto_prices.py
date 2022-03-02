1b3069aa1212c16961cd8b2bb26b7da45949e737
Dendi Suhubdy <gpuhashcracker@protonmail.com>
2022-03-02 14:41:20 +0700

    error: IndexError: Dimension out of range

--- skeleton/train_crypto_prices.py
+++ skeleton/train_crypto_prices.py
@@ -16,7 +16,7 @@ def build_lag_dataset(prices_data: list, lag: int):
         all the previous lags
     """
     train_data = []
-    # length_of_price_data = len(prices_data)
+    print(f"Total length of dataset is {len(prices_data)}")
     train_data = prices_data[lag:-lag]
     print(f"New timeseries is length is {len(train_data)}")
     return train_data
@@ -52,28 +52,10 @@ trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size)
 testset = torch.utils.data.TensorDataset(test_data)
 testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size)
 
-class LSTM(nn.Module):
-    def __init__(self, input_size=1, hidden_layer_size=100, output_size=1):
-        super().__init__()
-        self.hidden_layer_size = hidden_layer_size
-
-        self.lstm = nn.LSTM(input_size, hidden_layer_size)
-
-        self.linear = nn.Linear(hidden_layer_size, output_size)
-
-        self.hidden_cell = (torch.zeros(1,1,self.hidden_layer_size),
-                            torch.zeros(1,1,self.hidden_layer_size))
-
-    def forward(self, input_seq):
-        lstm_out, self.hidden_cell = self.lstm(input_seq.view(len(input_seq) ,1, -1), self.hidden_cell)
-        predictions = self.linear(lstm_out.view(len(input_seq), -1))
-        return predictions
-
 # Get cpu or gpu device for training.
-# device = "cuda" if torch.cuda.is_available() else "cpu"
-device = "cpu"
+device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Using {device} device")
-model = LSTM()
+model = nn.LSTM(1, 1)
 model = model.to(device)
 loss_function = nn.MSELoss()
 loss_function = loss_function.to(device)
